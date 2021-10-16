d7bf4fc05daf24da4ba3ad2b2b696923e68c6cc3
nicktheway <nikospa96@hotmail.gr>
2021-10-16 15:39:40 +0100

    Added Node type validation
    Make sure that the frame variable is owned by an AnimatedSprite node

--- diff_checker.sh
+++ diff_checker.sh
@@ -2,21 +2,88 @@
 # This script checks all the changed tracked files to check if the only difference is in the AnimatedSprite frame number.
 
 DRY_RUN=
+VERBOSE=
+
 
 function show_usage() {
 	echo "usage: $0 [-n]"
 	echo ''
 	echo '    -n: Dry run. Shows the files that will be restored.'
+	echo '    -v: Verbose. Explain the process, useful for debugging the script.'
 	echo ''
 }
 
-while getopts 'n' opt; do
+while getopts 'nv' opt; do
     case "$opt" in
-        n) DRY_RUN=t ;;
+        n) DRY_RUN=t; VERBOSE=t;;
+        v) VERBOSE=t ;;
         *) show_usage;
            exit 1
     esac
 done
+
+function log() {
+	if [ -z "$VERBOSE" ]; then
+		return 1
+	fi
+	echo "$@"
+}
+
+function validate_node_type() {
+	local filepath="$1"
+	local line="$2"
+
+	log '  > Verifying frame change on line' $line # for file $filepath
+	local stop=1
+	local previousline=$((line-1))
+	while [ $stop -eq 1 ]; do
+		local linetext=$(git diff -U$(wc -l "$filepath") | sed "${previousline}q;d") 
+		echo "$linetext" | grep "^ *$" > /dev/null
+		stop=$?
+		
+		echo "$linetext" | grep --extended-regexp "\[node.*\" type=\"AnimatedSprite\"" > /dev/null
+		if [ $? = 0 ]; then
+			log '    O Confirmed AnimatedSprite node: ' "$linetext"
+			return 0
+		fi
+
+		previousline=$((previousline-1))
+	done
+	return 1
+}
+
+function validate_file_frame_changes() {
+	local filepath="$1"
+	local framelines="$(git diff -U$(wc -l "$filepath") | grep -n '^[+-]frame' | cut -d':' -f1)"
+	for line in $(echo "$framelines"); do
+		validate_node_type "$filepath" "$line"
+		if [ $? -ne 0 ]; then
+			return 1
+		fi
+	done
+	return 0
+}
+
+function validate_revertable_changes() {
+	for filepath in "$@"; do
+		log ""
+		log "- Validating: " "$filepath"
+		validate_file_frame_changes "$filepath"
+		if [ $? -ne 0 ]; then
+			log 'The script will not revert this file as it looks like it contains important changes!'
+			continue
+		fi
+
+		if [ -z "$DRY_RUN" ]; then
+			git restore -- "$filepath"
+			log '  Restored changes.'
+		else
+			log '  DRY RUN MODE - Did not restore changes.'
+		fi
+
+	done
+	return 0
+}
 
 CHANGED_FILES="$(git diff --name-only | fmt -w1)"
 CHANGED_SCENES=$(echo "$CHANGED_FILES" | grep \.tscn$)
@@ -31,9 +98,8 @@ for FILE in $CHANGED_SCENES; do
 done
 
 if [ -n "$ONLY_FRAME_CHANGED_FILES" ]; then
-	if [ -z "$DRY_RUN" ]; then
-		echo "${ONLY_FRAME_CHANGED_FILES[@]}" | fmt -w1 | xargs -l bash -c 'git restore -- "$0"'
-	else
-		echo "${ONLY_FRAME_CHANGED_FILES[@]}"
-	fi
+	log 'Identified files with only frame changes: '
+	log ''
+	for file in ${ONLY_FRAME_CHANGED_FILES[@]}; do log "$file"; done;
+	validate_revertable_changes "${ONLY_FRAME_CHANGED_FILES[@]}"
 fi
